2cea5449476784ea5ce0a5d98b2c6df76e0fd154
CamiloSanPor <camilosanpor@gmail.com>
2024-08-17 12:29:51 -0300

    climbing work in progress lol

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,14 +6,20 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	var collision_info = move_and_collide(velocity * delta)
+	
+	if collision_info:
+		print("preent")
+		print(collision_info.get_collider())
+		if collision_info.get_collider() is TileMapLayer:
+			velocity.y = JUMP_VELOCITY
+	
+	gravity(delta)
+	walk()
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+func walk():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -22,4 +28,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func gravity(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
