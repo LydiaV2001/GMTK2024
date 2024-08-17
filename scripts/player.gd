6e7d5c6646be4a2cd69a3ac1d30385819a9212bd
CamiloSanPor <camilosanpor@gmail.com>
2024-08-17 13:14:51 -0300

    we can climb!

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,25 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var can_climb = false
 
 func _physics_process(delta):
-	
-	var collision_info = move_and_collide(velocity * delta)
-	
-	if collision_info:
-		print("preent")
-		print(collision_info.get_collider())
-		if collision_info.get_collider() is TileMapLayer:
-			velocity.y = JUMP_VELOCITY
-	
 	gravity(delta)
 	walk()
+	climb()
 	move_and_slide()
 
 func walk():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -32,3 +25,19 @@ func gravity(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+func climb():
+	if can_climb:
+		var direction = Input.get_axis("move_up", "move_down")
+		if direction:
+			velocity.y = direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.x, 0, SPEED)
+
+func _on_area_2d_body_entered(tile):
+	if tile is TileMapLayer:
+		can_climb = true
+
+func _on_area_2d_body_exited(tile):
+		if tile is TileMapLayer:
+			can_climb = false
