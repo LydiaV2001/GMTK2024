5ef48a569697eb0047e30b8da20e81caa6b08927
MmmGarlicFingers <lpunyu13@gmail.com>
2024-08-17 13:21:29 -0300

    Silly_Commit
    Stays on grid now!!

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,25 +1,42 @@
 extends TileMapLayer
+
+class_name Block
+
 @onready var timer = $Timer
-@export var gravity_direction : Vector2i = Vector2i(0, -1)
+@export var gravity_direction : Vector2i = Vector2i(0, 1)
+
+var x_position
+var y_position
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	x_position = position.x
+	y_position = position.y
 	timer.start(0.5)
 
-func _physics_process(delta):
-	pass
+func _physics_process(_delta):
+	position.x = x_position
+	position.y = y_position
 
 func _input(event):
 	#CHANGE THIS TO ALSO WORK WITH CONTROLLER!!!!!!!!!
 	
-	if event is InputEventMouseButton:
-		print("working")
-		#if event == MOUSE_BUTTON_LEFT:
-		#	rotate(-PI/2.)
-		#elif event.keycode == MOUSE_BUTTON_RIGHT:
-		#	rotate(PI/2.)
-			
-
+	if event.is_action_pressed("block_right"):
+		block_move(Vector2i(1, 0))
+	if event.is_action_pressed("block_left"):
+		block_move(Vector2i(-1, 0))
 
 func _on_timer_timeout():
+	block_move(gravity_direction)
+
+func block_move(v):
+	var tile_position = floor(position/32)*32;
 	var tween = self.create_tween()
-	tween.tween_property(self, "position", position - Vector2(gravity_direction*tile_set.tile_size), 0.2)
+	if v == gravity_direction:
+		tween.tween_property(self, "y_position", tile_position.y + Vector2(v*tile_set.tile_size).y, 0.1)
+	else:
+		tween.tween_property(self, "x_position", tile_position.x + Vector2(v*tile_set.tile_size).x, 0.1)
+
+
+func block_rotate(r):
+	var tween = self.create_tween()
+	tween.tween_property(self, "rotation", rotation - r, 0.1)
