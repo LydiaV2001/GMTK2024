6ad2bb060b526920cc8439d798a443a2e6645053
MmmGarlicFingers <lpunyu13@gmail.com>
2024-08-19 23:48:46 -0300

    Rudimentary Level Transitions

--- scripts/control_scripts/game_manager.gd
+++ scripts/control_scripts/game_manager.gd
@@ -2,11 +2,22 @@ extends Node
 class_name GameplayManager
 
 # array of levels
-var levels = ["res://scenes/levels/level_1.tscn", "res://scenes/levels/camilos_super_cool_level_way_better_than_jonahs.tscn", "res://scenes/levels/jonahs_better_levels_better_than_camilos_1.tscn"]
+var levels = ["res://scenes/levels/lvl_1.tscn",
+	"res://scenes/levels/lvl_2.tscn", 
+	"res://scenes/levels/lvl_3.tscn",
+	"res://scenes/levels/lvl_4.tscn",
+	"res://scenes/levels/lvl_5.tscn",
+	"res://scenes/levels/lvl_6.tscn",
+	"res://scenes/levels/lvl_7.tscn"
+	]
 
 # index of current level
 var index: int = 0;
 
+# load next level
+func next_level():
+	get_tree().change_scene_to_file(levels[index])
+	index += 1
 # index of current song value
 ## TODO; need to figure out why the FMod event doesn't like to take parameters.
 var current_music_index = "Track C"#: int = 1;
--- scripts/goal.gd
+++ scripts/goal.gd
@@ -41,5 +41,5 @@ func _on_body_entered(body):
 
 func _on_timer_timeout() -> void:
 	# load the next stage
-	get_tree().change_scene_to_file(GameManager.levels[1])
+	GameManager.next_level()
 	pass # Replace with function body.
